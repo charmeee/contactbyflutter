e36fc18bcffdb815c272749d831bad6fb04ad925
dyi919 <ehddjs13579@gmail.com>
2022-04-26 02:28:10 +0900

    initial directory settings and reset css

---
+++ umm-whoru/src/components/SearchBar.js
@@ -0,0 +1,9 @@
+function SearchBar() {
+  return (
+    <div>
+      <p>Search Bar</p>
+    </div>
+  );
+}
+
+export default SearchBar;
---
+++ umm-whoru/src/routes/MyProfile.js
@@ -0,0 +1,15 @@
+import "../styles/styles.scss";
+
+import SearchBar from "../components/SearchBar";
+import SideBar from "../components/SideBar";
+
+function MyProfile() {
+  return (
+    <div>
+      <SearchBar />
+      <SideBar />
+    </div>
+  );
+}
+
+export default MyProfile;
