3f98c342e395183394601a918ebdf685da69b204
dyi919 <ehddjs13579@gmail.com>
2022-04-28 08:29:29 +0900

    added profile info

--- umm-whoru/src/components/Profile.js
+++ umm-whoru/src/components/Profile.js
@@ -4,32 +4,53 @@ import { faPhone, faAt, faComment } from "@fortawesome/free-solid-svg-icons";
 function Profile() {
   return (
     <div className="profile-wrapper">
-      <div className="profile-upper h-2/5">
+      <div className="profile-upper h-1/2">
         <p>Profile pic + Name + Introduction</p>
       </div>
-      <div className="profile-lower h-3/5 w-4/5 mx-auto">
+      <div className="profile-lower h-1/2 w-4/5 mx-auto">
         <div className="profile-button-wrapper">
           <div className="flex items-center justify-center">
             <button
-              className="bg-main-3 w-1/2 h-16 text-lg font-semibold border-b-4 border-main-3 text-main-0"
+              className="bg-main-3 w-1/2 h-16 text-xl font-semibold border-b-4 border-main-3 text-main-0"
               active="true"
             >
               Profile
             </button>
-            <button className="active:bg-main-3 w-1/2 h-16 text-lg font-semibold border-b-4 border-main-3 text-main-0">
+            <button className="active:bg-main-3 w-1/2 h-16 text-xl font-semibold border-b-4 border-main-3 text-main-0">
               Friends
             </button>
           </div>
         </div>
         <div className="profile-info-wrapper">
-          <div className="info-row">
-            <FontAwesomeIcon icon={faPhone} />
+          <div className="info-row grid grid-cols-6 p-5 pt-7 border-solid border-b-2 border-main-3">
+            <FontAwesomeIcon
+              className="text-main-0 p-4"
+              icon={faPhone}
+              size="2x"
+            />
+            <span className="col-span-5 p-5 text-xl font-semibold text-main-0">
+              010-4385-3025
+            </span>
           </div>
-          <div className="info-row">
-            <FontAwesomeIcon icon={faAt} />
+          <div className="info-row grid grid-cols-6 p-5 border-solid border-b-2 border-main-3">
+            <FontAwesomeIcon
+              className="text-main-0 p-4"
+              icon={faAt}
+              size="2x"
+            />
+            <span className="col-span-5 p-4 text-xl font-semibold text-main-0">
+              ydu0919@gmail.com
+            </span>
           </div>
-          <div className="info-row">
-            <FontAwesomeIcon icon={faComment} />
+          <div className="info-row grid grid-cols-6 p-4 border-solid border-b-2 border-main-3">
+            <FontAwesomeIcon
+              className="text-main-0 p-4"
+              icon={faComment}
+              size="2x"
+            />
+            <span className="col-span-5 p-4 text-xl font-semibold text-main-0">
+              ydu0919
+            </span>
           </div>
         </div>
       </div>
