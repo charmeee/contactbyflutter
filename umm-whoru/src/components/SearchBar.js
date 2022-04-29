b5728b3019c896c3e2b21a579930b4bebc43ad68
KwonSoonHyeon <skysoon02@korea.ac.kr>
2022-04-30 00:14:04 +0900

    SearchBar

--- umm-whoru/src/components/SearchBar.js
+++ umm-whoru/src/components/SearchBar.js
@@ -1,8 +1,15 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {  faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+
+import "./SearchBar.css";
+
 function SearchBar() {
   return (
-    <div className="searchbar-wrapper">
-      <p>Search Bar</p>
-    </div>
+    <div id="SearchBar">
+      <a id="SearchBarIcon" href="/search" onClick={(event)=>{
+        event.preventDefault();
+      }}><FontAwesomeIcon icon={faMagnifyingGlass}/></a>
+  </div>
   );
 }
 
