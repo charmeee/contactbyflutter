e83e7cdedc2479b344b78d52b2f96f0770f474f4
jw0111 <59572441+jw0111@users.noreply.github.com>
2022-04-30 19:43:38 +0900

    editProfile

--- umm-whoru/src/App.js
+++ umm-whoru/src/App.js
@@ -1,24 +1,29 @@
-import { browserSessionPersistence, onAuthStateChanged, setPersistence } from "firebase/auth";
+import {
+  browserSessionPersistence,
+  onAuthStateChanged,
+  setPersistence,
+} from "firebase/auth";
 import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { auth } from "./firebase.config";
+import EditProfile from "./routes/EditProfile";
 import MyProfile from "./routes/MyProfile";
 
 function App() {
-  const [isAuth,setIsAuth] = useState(false);
-  setPersistence(auth, browserSessionPersistence).then(()=>{});
-  onAuthStateChanged(auth,(currentUser)=>{
-    if(currentUser){
+  const [isAuth, setIsAuth] = useState(false);
+  setPersistence(auth, browserSessionPersistence).then(() => {});
+  onAuthStateChanged(auth, (currentUser) => {
+    if (currentUser) {
       setIsAuth(true);
-    }
-    else{
+    } else {
       setIsAuth(false);
     }
-  })
+  });
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<MyProfile isAuth={isAuth}/>} />
+        <Route path="/" element={<MyProfile isAuth={isAuth} />} />
+        <Route path="/profile" element={<EditProfile isAuth={isAuth} />} />
       </Routes>
     </Router>
   );
